6b90b8ed0590dd62f5974b2c9ea894f378dc6018
Bharat Bhushan <239651116+program-bharat@users.noreply.github.com>
2026-04-04 23:11:52 +0530

    feat: add update and delete vehicle functionality for owner

--- Backend/src/controllers/vehicleController.js
+++ Backend/src/controllers/vehicleController.js
@@ -2,8 +2,8 @@ const Vehicle = require('../models/Vehicles');
 
 exports.getVehicles = async (req, res) => {
     try {
-        const vehicles = await Vehicle.find();
-        res.json(vehicles);
+        const vehicles = await Vehicle.find({ ownerId: req.user.id });
+        res.json({ message: `Owner Vehicles Fetched Successfully`, vehicles });
     } catch (error) {
         console.log(error);
         res.status(500).json({ error: error.message });
@@ -23,15 +23,44 @@ exports.createVehicle = async (req, res) => {
 }
 exports.updateVehicle = async (req, res) => {
     try {
-
+        const vehicleID = req.params.id;
+        const vehicle = await Vehicle.findById(vehicleID);
+        if (!vehicle) {
+            return res.status(404).json({ message: "Vehicle Not Found" });
+        }
+        // Check OwnerShip
+        if (vehicle.ownerId.toString() !== req.user.id) {
+            return res.status(403).json({ message: "Not Authorized" });
+        }
+        const updatedVehicle = await Vehicle.findByIdAndUpdate(
+            vehicleID,
+            req.body,
+            { new: true }
+        );
+        res.status(200).json({
+            message: "Vehicle Updated Successfully",
+            vehicle: updatedVehicle
+        });
     } catch (error) {
-
+        console.log(error);
+        res.status(500).json({ error: error.message });
     }
 }
 exports.deleteVehicle = async (req, res) => {
     try {
-
+        const vehicleID = req.params.id;
+        const vehicle = await Vehicle.findById(vehicleID);
+        if (!vehicle) {
+            return res.status(404).json({ message: "Vehicle Not Found" });
+        }
+        // Check OwnerShip
+        if (vehicle.ownerId.toString() !== req.user.id) {
+            return res.status(403).json({ message: "Not Authorized" });
+        }
+        await Vehicle.findByIdAndDelete(vehicleID);
+        res.status(200).json({ message: "Vehicle Deleted Successfully" });
     } catch (error) {
-
+        console.log(error);
+        res.status(500).json({ error: error.message });
     }
 }
--- Backend/src/routes/vehicleRoute.js
+++ Backend/src/routes/vehicleRoute.js
@@ -6,7 +6,7 @@ const { protect, authorize } = require('../middlewares/authMiddleware');
 
 router.get("/", protect, authorize('owner'), getVehicles);
 router.post("/", protect, authorize('owner'), createVehicle);
-router.put("/:id", updateVehicle);
-router.delete("/:id", deleteVehicle);
+router.put("/:id", protect, authorize('owner'), updateVehicle);
+router.delete("/:id", protect, authorize('owner'), deleteVehicle);
 
 module.exports = router;
