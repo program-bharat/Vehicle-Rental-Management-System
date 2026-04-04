ff261adbdecad8e58e9704e207ef5a65f6659530
Bharat Bhushan <239651116+program-bharat@users.noreply.github.com>
2026-04-04 23:16:52 +0530

    feat: implement admin controls for verifying users and approving vehicles

--- Backend/src/controllers/userController.js
+++ Backend/src/controllers/userController.js
@@ -1,4 +1,5 @@
 const User = require('../models/Users');
+const Vehicle = require('../models/Vehicles')
 
 exports.getAllUsers = async (req, res) => {
     try {
@@ -21,6 +22,39 @@ exports.userToOwner = async (req, res) => {
             return res.status(404).json({ message: 'User not found' });
         }
         res.status(200).json({ message: `User Promoted to Owner`, user });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: error.message });
+    }
+}
+
+exports.verifyUser = async (req, res) => {
+    try {
+        const user = await User.findByIdAndUpdate(
+            req.params.id,
+            { isVerified: true },
+            { new: true }
+        )
+        if (!user) {
+            return res.status(404).json({ message: "User Not Found" });
+        }
+        res.status(200).json({ message: "User verified successfully", user })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: error.message });
+    }
+}
+exports.approveVehicle = async (req, res) => {
+    try {
+        const vehicle = await Vehicle.findByIdAndUpdate(
+            req.params.id,
+            { isApproved: true },
+            { new: true }
+        )
+        if (!vehicle) {
+            return res.status(404).json({ message: "Vehicle Not Found" });
+        }
+        res.status(200).json({ message: "Vehicle Approved Successfully", vehicle })
     } catch (error) {
         console.log(error);
         res.status(500).json({ error: error.message });
--- Backend/src/routes/userRoute.js
+++ Backend/src/routes/userRoute.js
@@ -2,11 +2,17 @@ const express = require('express');
 const router = express.Router();
 
 const { protect, authorize } = require('../middlewares/authMiddleware');
-const { userToOwner, getAllUsers } = require('../controllers/userController');
+const { userToOwner, getAllUsers, verifyUser, approveVehicle } = require('../controllers/userController');
 
 // Admin can see all the users
 router.get("/", protect, authorize('admin'), getAllUsers);
 // Make user -> owner (only Admin can promote)
 router.put("/make-owner/:id", protect, authorize('admin'), userToOwner);
+
+// User --> isVerified: false->true
+router.put("/verify/:id", protect, authorize('admin'), verifyUser);
+
+// Vehicle --> isApproved: false->true
+router.put("/approveVehicle/:id", protect, authorize('admin'), approveVehicle);
 
 module.exports = router;
